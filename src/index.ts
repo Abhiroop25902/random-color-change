290bb45485ee0397fcd1da92f03929cf20493c34
Abhiroop Mukherjee <abhiroop.mukherjee@oraclec.om>
2023-09-25 11:03:40 +0530

    end result

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+const COLOR_CHAR_LIST = [
+    ...Array.from(Array(10).keys()).map((a) => `${a}`),
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+];
+
+function generateRandomColor(){
+    let color = '#';
+
+    for(let i =0; i < 6; i++){
+        let randomIndex = Math.floor(Math.random() * COLOR_CHAR_LIST.length)
+        color += COLOR_CHAR_LIST[randomIndex];
+    }
+
+    return color
+}
+
+
+function changeBackGroundColor() {
+    document.body.style.backgroundColor = generateRandomColor();
+}
+
+changeBackGroundColor();
